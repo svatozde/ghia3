c9a670d1228333ed5d6af92bdb61f95dd864515c
Zdenek Svaton <zdenek.svaton@blindspot.ai>
2019-12-03 01:18:50 +0100

    my tests added

---
+++ my_tests/test.py
@@ -0,0 +1,77 @@
+from unittest import TestCase
+from ghia.github import GitHub
+from betamax import Betamax
+from requests import Session
+from betamax.cassette import cassette
+import os
+import pathlib
+from ghia.common import GHIA,get_rules
+import configparser
+
+TOKEN_PLACEHOLDER = '<AUTH_TOKEN>'
+TOKEN = os.getenv('GITHUB_TOKEN', default=TOKEN_PLACEHOLDER)
+
+USER_PLACEHOLDER = 'USER_PLACEHOLDER'
+#USER = os.getenv('GITHUB_USER', default=USER_PLACEHOLDER)
+USER = 'USER_PLACEHOLDER'
+TOKEN_HEADER = 'token ' + TOKEN
+
+REPO = 'ghia_test_env'
+
+
+def sanitize_token(interaction, current_cassette):
+    headers = interaction.data['request']['headers']
+    token = headers.get("Authorization")
+    if token is None:
+        return
+
+    current_cassette.placeholders.append(
+        cassette.Placeholder(placeholder=TOKEN_PLACEHOLDER, replace=token[0])
+        )
+
+    current_cassette.placeholders.append(
+        cassette.Placeholder(placeholder=USER_PLACEHOLDER, replace=USER)
+        )
+
+
+with Betamax.configure() as config:
+    config.cassette_library_dir = 'cassettes'
+    config.define_cassette_placeholder(TOKEN_PLACEHOLDER, TOKEN_HEADER)
+    config.before_record(callback=sanitize_token)
+
+
+def configPath(name):
+    return pathlib.Path(__file__).parent / 'rules' / name
+
+
+
+class TestGit(TestCase):
+
+    def test_list(self):
+        github = GitHub(TOKEN)
+        with Betamax(github.session).use_cassette('issues'):
+            issues = github.issues(USER, REPO)
+            assert len(issues) == 116
+
+
+    def test_assign(self):
+        github = GitHub(TOKEN)
+        with Betamax(github.session).use_cassette('assignees'):
+            github.set_issue_assignees(USER, REPO,124,[USER])
+            issues = github.issues(USER, REPO, assignee=USER)
+            for issue in issues:
+                if issue['number'] == 124:
+                    assert issue['assignees'][0]['login'] == USER
+                    break
+
+    def test_apply_rules(self):
+        with open(configPath('rules.match_label.cfg'),'r') as conf:
+            rules, fallback  = get_rules(None,None,conf)
+            ghia = GHIA(TOKEN, rules, fallback, False, GHIA.DEFAULT_STRATEGY)
+
+            with Betamax(ghia.github.session).use_cassette('test_apply_rules'):
+                ghia.run(USER, REPO)
+                issues = ghia.github.issues(USER, REPO, assignee=USER)
+                assert len(issues) == 14
+
+
--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
 import setuptools
 import codecs
+import unittest
+import my_tests
 from glob import glob
 
-
-with codecs.open("README.md",encoding='utf-8') as fh:
+with codecs.open("README.md", encoding='utf-8') as fh:
     long_description = fh.read()
 
-with codecs.open("LICENSE",encoding='utf-8') as fh:
+with codecs.open("LICENSE", encoding='utf-8') as fh:
     license = fh.read()
-
 
 packages = setuptools.find_packages(include=['ghia'])
 
+
+def my_test_suite():
+    print("TEST SUITE INIT")
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.loadTestsFromModule(my_tests)
+    print(test_suite.countTestCases())
+    return test_suite
+
+
 setuptools.setup(
-    name="ghia_svatozde", # Replace with your own username
-    version="0.0.17",
+    name="ghia_svatozde",  # Replace with your own username
+    version="0.0.20",
     author="Zdenek Svaton",
     author_email="author@example.com",
     description="ghia webapp",
@@ -28,22 +37,29 @@ setuptools.setup(
         'Programming Language :: Python :: 3.7',
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
+        ],
     install_requires=[
         'flask',
         'click',
         'requests',
-    ],
-    license = license,
+        ],
+    license=license,
     python_requires='>=3.6',
     packages=packages,
     data_files=[
         ('', glob('data/**')),
         ('', glob('LICENSE'))
-    ],
+        ],
     entry_points={  # Optional
         'console_scripts': [
             'ghia=ghia.__main__:main',
+            ],
+        },
+    tests_require=[
+        'nose',
+        'coverage',
+        'betamax',
+        'pytest',
         ],
-    },
-)
+    test_suite='nose.collector',
+    )
