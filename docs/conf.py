1fc6f5a07e18292e710ff6363f2f34984130f919
Zdenek Svaton <zdenek.svaton@blindspot.ai>
2019-12-09 19:25:28 +0100

    requireents added my_test excluded form documentation

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../ghia'))
-sys.path.insert(0, os.path.abspath('../my_test'))
 
 
 # -- Project information -----------------------------------------------------
@@ -33,7 +32,6 @@ release = '0.4'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -42,7 +40,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','docs']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','docs', 'my_tests']
 
 
 # -- Options for HTML output -------------------------------------------------
