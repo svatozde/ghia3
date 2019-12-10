ed3e7c56dabf4f7cbd300bc32130d79d2c92c319
Zdenek Svaton <zdenek.svaton@blindspot.ai>
2019-12-10 10:09:27 +0100

    sphinx extensions added

--- docs/_build/html/_sources/index.rst.txt
+++ docs/_build/html/_sources/index.rst.txt
@@ -11,7 +11,6 @@ Welcome to ghia's documentation!
 
    README
    ghia
-   my_tests
 
 Indices and tables
 ==================
--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,11 @@ release = '0.4'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-]
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest'
+    ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -40,7 +44,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','docs', 'my_tests']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','docs']
 
 
 # -- Options for HTML output -------------------------------------------------
